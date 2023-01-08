35f00dcf86e786be87c708400365ca664319d04f
Magnus Burenius <magnus.burenius@gmail.com>
2023-01-08 17:27:19 +0100

    typescript: add hello world

---
+++ typescript/main.ts
@@ -0,0 +1,12 @@
+function capitalize(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }
+  
+  function hello(name) {
+    return "Hello " + capitalize(name);
+  }
+  
+  console.log(hello("john"));
+  console.log(hello("Sarah"));
+  console.log(hello("kai"));
+  
