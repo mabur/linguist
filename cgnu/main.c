1694e4adfbe4bfb8b0e20fdbda67f8b80303d543
Magnus Burenius <magnus.burenius@gmail.com>
2023-07-17 12:44:23 +0200

    cnu: comment what is gnuc specific

--- cgnu/main.c
+++ cgnu/main.c
@@ -5,7 +5,7 @@
 #define MIN(a,b) (((a)<(b))?(a):(b))
 #define MAX(a,b) (((a)>(b))?(a):(b))
 
-#define let __auto_type
+#define let __auto_type // Requires GNUC. C23 also has auto.
 
 #define INIT_RANGE(range, count) do { \
     (range).first = malloc((count) * sizeof(*(range).first)); \
