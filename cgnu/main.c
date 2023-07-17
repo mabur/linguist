f4ec30f7218e96e7a1dd9c52a3a99b4d15ce81e6
Magnus Burenius <magnus.burenius@gmail.com>
2023-07-17 12:07:10 +0200

    cgnu:refactor: extract macro INIT_RANGE

--- cgnu/main.c
+++ cgnu/main.c
@@ -7,6 +7,11 @@
 
 #define let __auto_type
 
+#define INIT_RANGE(range, count) do { \
+    range.first = malloc(count * sizeof(*range.first)); \
+    range.last = range.first + count; \
+} while (0)
+
 typedef struct {
     double x;
     double y;
@@ -90,8 +95,7 @@ World makeWorld() {
     let world = (World){};
 
     let num_spheres = 5;
-    world.spheres.first = malloc(num_spheres * sizeof(Sphere));
-    world.spheres.last = world.spheres.first + num_spheres;
+    INIT_RANGE(world.spheres, num_spheres);
     world.spheres.first[0] = (Sphere){(Vec3d){-2, 0, 6}, 1, (Vec3d){MAX_C, MAX_C, MIN_C}};
     world.spheres.first[1] = (Sphere){(Vec3d){0, 0, 5}, 1, (Vec3d){MAX_C, MIN_C, MIN_C}};
     world.spheres.first[2] = (Sphere){(Vec3d){2, 0, 4}, 1, (Vec3d){2 * MIN_C, 4 * MIN_C, MAX_C}};
@@ -99,8 +103,7 @@ World makeWorld() {
     world.spheres.first[4] = (Sphere){(Vec3d){0, -1 - R, 0}, R * R, (Vec3d){MAX_C, MAX_C, MAX_C}};
 
     let num_lights = 2;
-    world.lights.first = malloc(num_lights * sizeof(Light));
-    world.lights.last = world.lights.first + num_lights;
+    INIT_RANGE(world.lights, num_lights);
     world.lights.first[0] = (Light){(Vec3d){+1, +1, +2}, muls(0.4, (Vec3d){1, 0.8, 0.5})};
     world.lights.first[1] = (Light){(Vec3d){-1, -1, -2}, muls(0.4, (Vec3d){0.5, 0.5, 1})};
     
