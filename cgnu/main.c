6e7c7c12094da58938f363d7dd9b54c261e1e94e
Magnus Burenius <magnus.burenius@gmail.com>
2025-07-26 12:53:18 +0200

    refactor: array macros

--- cgnu/main.c
+++ cgnu/main.c
@@ -2,24 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define let __auto_type // Requires GNUC. C23 also has auto.
-
 typedef double Vec3d __attribute__((vector_size(32))); // gcc/clang vector extensions
 
-#define INIT_RANGE(range, count) do { \
-    (range).first = malloc((count) * sizeof(*(range).first)); \
-    (range).last = (range).first + (count); \
+#define let __auto_type // Requires GNUC. C23 also has auto.
+
+#define INIT_RANGE(range, mycount) do { \
+    (range).data = malloc((mycount) * sizeof(*(range).data)); \
+    (range).count = (mycount); \
 } while (0);
 
 #define FREE_RANGE(range) do { \
-    free((range).first); \
-    (range).first = NULL; \
-    (range).last = NULL; \
+    free((range).data); \
+    (range).data = NULL; \
+    (range).count = 0; \
 } while (0);
 
-#define FOR_RANGE(it, range) \
-    for (__auto_type it = (range).first; it != (range).last; ++it)
-
+#define FOR_EACH(it, range) \
+    for (let it = (range).data; it < (range).data + (range).count; ++it)
 
 double dot(Vec3d a, Vec3d b) {
     return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
@@ -49,13 +48,13 @@ typedef struct {
 } Light;
 
 typedef struct {
-    Sphere* first;
-    Sphere* last;    
+    Sphere* data;
+    size_t count;
 } Spheres;
 
 typedef struct {
-    Light* first;
-    Light* last;
+    Light* data;
+    size_t count;
 } Lights;
 
 typedef struct {
@@ -82,15 +81,15 @@ World makeWorld() {
     let world = (World){};
     
     INIT_RANGE(world.spheres, 5);
-    world.spheres.first[0] = (Sphere){(Vec3d){-2, 0, 6}, 1, (Vec3d){MAX_C, MAX_C, MIN_C}};
-    world.spheres.first[1] = (Sphere){(Vec3d){0, 0, 5}, 1, (Vec3d){MAX_C, MIN_C, MIN_C}};
-    world.spheres.first[2] = (Sphere){(Vec3d){2, 0, 4}, 1, (Vec3d){2 * MIN_C, 4 * MIN_C, MAX_C}};
-    world.spheres.first[3] = (Sphere){(Vec3d){0, 1 + R, 0}, R * R, (Vec3d){MIN_C, MAX_C, MIN_C}};
-    world.spheres.first[4] = (Sphere){(Vec3d){0, -1 - R, 0}, R * R, (Vec3d){MAX_C, MAX_C, MAX_C}};
+    world.spheres.data[0] = (Sphere){(Vec3d){-2, 0, 6}, 1, (Vec3d){MAX_C, MAX_C, MIN_C}};
+    world.spheres.data[1] = (Sphere){(Vec3d){0, 0, 5}, 1, (Vec3d){MAX_C, MIN_C, MIN_C}};
+    world.spheres.data[2] = (Sphere){(Vec3d){2, 0, 4}, 1, (Vec3d){2 * MIN_C, 4 * MIN_C, MAX_C}};
+    world.spheres.data[3] = (Sphere){(Vec3d){0, 1 + R, 0}, R * R, (Vec3d){MIN_C, MAX_C, MIN_C}};
+    world.spheres.data[4] = (Sphere){(Vec3d){0, -1 - R, 0}, R * R, (Vec3d){MAX_C, MAX_C, MAX_C}};
     
     INIT_RANGE(world.lights, 2);
-    world.lights.first[0] = (Light){(Vec3d){+1, +1, +2}, 0.4 * (Vec3d){1, 0.8, 0.5}};
-    world.lights.first[1] = (Light){(Vec3d){-1, -1, -2}, 0.4 * (Vec3d){0.5, 0.5, 1}};
+    world.lights.data[0] = (Light){(Vec3d){+1, +1, +2}, 0.4 * (Vec3d){1, 0.8, 0.5}};
+    world.lights.data[1] = (Light){(Vec3d){-1, -1, -2}, 0.4 * (Vec3d){0.5, 0.5, 1}};
     
     world.atmosphere_color = 0.3 * (Vec3d){0.5, 0.5, 1};
     return world;
@@ -123,7 +122,7 @@ Intersection findSingleIntersection(
 
 Intersection findIntersection(Vec3d start, Vec3d direction, Spheres spheres) {
     let i1 = makeIntersection();
-    FOR_RANGE(sphere, spheres) {
+    FOR_EACH(sphere, spheres) {
         let i2 = findSingleIntersection(start, direction, *sphere);
         if (i2.distance < i1.distance) {
             i1 = i2;
@@ -146,7 +145,7 @@ Vec3d shade(Intersection intersection, World world) {
         return (Vec3d){1, 1, 1};
     }
     let color = shadeAtmosphere(intersection, world.atmosphere_color);
-    FOR_RANGE(light, world.lights) {
+    FOR_EACH(light, world.lights) {
         color += shadeSingleLight(intersection, *light);
     }
     return color;
